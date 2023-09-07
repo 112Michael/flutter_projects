7dcec2d5012eedbd34ecf3b7fb4785466a1b9f02
112Michael <136795135+112Michael@users.noreply.github.com>
2023-09-08 00:29:09 +0530

    initial commit

--- lib/simple splash.dart
+++ lib/simple splash.dart
@@ -1,24 +1,27 @@
-import'package:flutter/material.dart';
-void main()
-{
-  runApp(MaterialApp
+import 'package:flutter/material.dart';
 
-    (home: Splashsimple()));
+void main() {
+  runApp(MaterialApp(home: Splashsimple()));
 }
-class Splashsimple extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(backgroundColor:Colors.black12
-      ,
-      body:Center(
-        child: Column(mainAxisAlignment:MainAxisAlignment.center,
-          children: [
-          Image.asset('asset/icon/spottify.png',height:80,width:80),
 
-        Text("spotify",style:TextStyle(fontSize:50,fontWeight:FontWeight.bold,
-        color:Colors.white38,
-        ),),]
-    ),
-      ),);
+class Splashsimple extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset('asset/icon/spottify.png', height: 80, width: 80),
+          const Text(
+            "spotify",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Colors.white38,
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
