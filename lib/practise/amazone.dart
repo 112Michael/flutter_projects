4aafb38709b6974db3462cd5c4a5c537f22cf8d5
112Michael <136795135+112Michael@users.noreply.github.com>
2023-09-08 19:00:08 +0530

    initial commit

--- lib/practise/amazone.dart
+++ lib/practise/amazone.dart
@@ -8,18 +8,26 @@ class Amazone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Image.asset("asset/icon/amazone.png", height: 60, width: 60),
-          const Text(
-            "amazone",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black,
+      backgroundColor: Colors.white,//set background way one
+      body: Container(
+        //2nd way to adding background
+        decoration:const BoxDecoration(
+        image:DecorationImage(
+            fit:BoxFit.cover,
+            image: AssetImage("asset/image/IMG_20230126_194655.jpg"))
+        ),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset("asset/icon/amazone.png", height: 60, width: 60),
+            const Text(
+              "amazone",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black,
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
